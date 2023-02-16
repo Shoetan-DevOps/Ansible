5988a65107f5b87dc7e521142402e1af7e9b0e8e
ashoetan <alfredshoetan@gmail.com>
2023-02-16 23:28:07 +0000

    draft k8s roles & complete ansible shortcuts

---
+++ bash_aliases-ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Create ansible aliases for commonly used commands"
+
+cat << 'EOF' > /home/ansible/.bash_aliases
+alias lkm="ls /etc/ansible/roles/kube-master/"
+alias lkw="ls /etc/ansible/roles/kube-worker/"
+alias ekmt="vi /etc/ansible/roles/kube-master/tasks/main.yml"
+alias ekwt="vi /etc/ansible/roles/kube-worker/tasks/main.yml"
+alias epl="vi /etc/ansible/roles/playbooks/playbook.yml"
+alias rpl="ansible-playbook /etc/ansible/playbooks/playbook.yml"
+alias drpl="ansible-playbook /etc/ansible/playbooks/playbook.yml --check"
+EOF
+
+echo "complete file creation"
+
+sudo chmod ugo+x /home/ansible/.bash_aliases
+
+echo "COMPLETE - NOW RUN COMMAND =====> 'source /home/ansible/.bash_aliases'"
